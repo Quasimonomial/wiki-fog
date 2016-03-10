6f64f3f6f238e619d75ade30740f841003fc5f29
Travis Herrick <quasimonomial@gmail.com>
2016-03-09 17:51:16 -0800

    updated blacklist and allow words weith apostrophies

--- app/services/wikipedia_service.rb
+++ app/services/wikipedia_service.rb
@@ -1,8 +1,10 @@
 class WikipediaService
   require 'wikipedia'
 
-  WORD_BLACKLIST = ["the", "and", "of", "to", "a", "is", "as", "with", "their", "by",
-    "or", "be", "on", "s", "at", "nt", "t", "f", "in", "for", "are"]
+  MOST_COMMON_WIKIPEDIA_WORDS = ["time","person","year","way","day","thing","man","world","life","hand","part","child","eye","woman","place","work","week","case","point","government","company","number","group","problem","fact","be","have","do","say","get","make","go","know","take","see","come","think","look","want","give","use","find","tell","ask","work","seem","feel","try","leave","call","good","new","first","last","long","great","little","own","other","old","right","big","high","different","small","large","next","early","young","important","few","public","bad","same","able","to","of","in","for","on","with","at","by","from","up","about","into","over","after","beneath","under","above","the","and","a","that","I","it","not","he","as","you","this","but","his","they","her","she","or","an","will","my","one","all","would","there","their"]
+
+  WORD_BLACKLIST = MOST_COMMON_WIKIPEDIA_WORDS + ["has", "also", "which", "s", "was", "are", "is", "if", "no", "like", "where", "being", "who", "them", "had", "however", "often", "while", "only", "many", "during", "between", "these", "when", "most", "such", "than", "some", "more", "been", "may", "cats", "were"]
+
 
   def do_the_thing
     article_title = 'Cat'
@@ -24,7 +26,7 @@ class WikipediaService
   def generate_word_frequency_hash text
     frequency_hash = Hash.new(0)
 
-    words = text.split(/\W+/)
+    words = text.split(/\W+|'/)
 
     words.each do |word|
       next if WORD_BLACKLIST.include?(word.downcase)
