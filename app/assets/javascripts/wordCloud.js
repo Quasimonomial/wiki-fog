e1efd084cd9b09e20f873a55725f05b588522eaa
Travis Herrick <quasimonomial@gmail.com>
2016-03-16 15:00:27 -0700

    add spinner while query is loading

--- app/assets/javascripts/wordCloud.js
+++ app/assets/javascripts/wordCloud.js
@@ -2,12 +2,29 @@
 window.onload = function(){
   cloudInitialized = false;
 
+  spinnerOpts = {
+    lines: 13,
+    length: 68,
+    width: 22,
+    radius: 54,
+    scale: 1,
+    color: ["#0cf", "#0cf", "#0cf", "#39d", "#90c5f0", "#90a0dd", "#90c5f0", "#a0ddff", "#99ccee", "#aab5f0"], // colors that the word cloud uses by default
+    opacity: 0.3,
+    speed: 0.5,
+    trail: 70,
+    className: 'spinner',
+    shadow: true
+  }
+
+  spinner = new Spinner(spinnerOpts)
+
   $('.article-form').submit(function(event) {
     event.preventDefault();
 
     data = $(this).serialize();
 
     $('.article-form :input').prop("disabled", true)
+    spinner.spin($('.word-cloud')[0])
 
     $.ajax({
       type: "POST",
@@ -22,9 +39,12 @@ window.onload = function(){
         } else {
           $('.word-cloud').jQCloud('update', response);
         }
-
-        $('.article-form :input').prop("disabled", false)
       },
+      complete: function(){
+        $('.article-form :input').prop("disabled", false)
+        spinner.stop()
+      },
+
       dataType: "json"
     });
   });
