1f3cfc58dd2ba47fd6e3d870e04657f998a39f5d
Travis Herrick <quasimonomial@gmail.com>
2016-03-16 13:04:05 -0700

    ask for 50 articles at per request instead of one at a time

--- app/services/wikipedia_service.rb
+++ app/services/wikipedia_service.rb
@@ -1,4 +1,5 @@
 class WikipediaService
+  require 'uri'
   require 'wikipedia'
 
   MOST_COMMON_WIKIPEDIA_WORDS = ["time","person","year","way","day","thing","man","world","life","hand","part","child","eye","woman","place","work","week","case","point","government","company","number","group","problem","fact","be","have","do","say","get","make","go","know","take","see","come","think","look","want","give","use","find","tell","ask","work","seem","feel","try","leave","call","good","new","first","last","long","great","little","own","other","old","right","big","high","different","small","large","next","early","young","important","few","public","bad","same","able","to","of","in","for","on","with","at","by","from","up","about","into","over","after","beneath","under","above","the","and","a","that","I","it","not","he","as","you","this","but","his","they","her","she","or","an","will","my","one","all","would","there","their"]
@@ -13,10 +14,19 @@ class WikipediaService
     text = page.text
     links = WikipediaService.fetch_links article_title
 
-    links.each do |link|
-      puts "fetching #{link}"
-      link_page = Wikipedia.find(link)
-      text += " #{link_page.text} "
+    links.each_slice(50) do |links|
+      puts "fetching #{links}"
+      api_link = URI.escape("http://en.wikipedia.org/w/api.php?action=query&format=json&rvprop=content&inprop=url&prop=extracts&explaintext=&titles=#{links.join('|')}")
+      response = HTTParty.get(api_link)
+      response["query"]["pages"].each do |id, article|
+        begin
+          article_text = article["extract"]
+          text += article_text
+        rescue
+          puts "problem fetching article #{id}"
+        end
+      end
+
     end
 
     format_word_frequency_array generate_word_frequency_hash text
