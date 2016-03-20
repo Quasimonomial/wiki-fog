111df1f08befd12051b6a2bd369818e5621e5fd6
Travis Herrick <quasimonomial@gmail.com>
2016-03-19 21:55:31 -0700

    add spec for toggle menu

--- spec/javascripts/wikifog.spec.js
+++ spec/javascripts/wikifog.spec.js
@@ -1,6 +1,12 @@
 describe("Wikifog", function() {
-  it("contains spec with an expectation", function() {
-    // loadFixtures("index.html");
-    expect(true).toBe(true);
+  describe("toggleMenu", function(){
+    it("opens and closes the menu", function(){
+      loadFixtures("index.html");
+      expect($('.dropdown-menu-content')).not.toHaveClass('hidden');
+      $('.dropdown-menu-button').click();
+      expect($('.dropdown-menu-content')).toHaveClass('hidden');
+      $('.dropdown-menu-button').click();
+      expect($('.dropdown-menu-content')).not.toHaveClass('hidden');
+    });
   });
 });
