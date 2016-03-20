60917c4eb95debae57d9f0dfb5fafcaed35a284c
Travis Herrick <quasimonomial@gmail.com>
2016-03-19 22:22:35 -0700

    error specs for wikifog.js

--- spec/javascripts/wikifog.spec.js
+++ spec/javascripts/wikifog.spec.js
@@ -1,7 +1,39 @@
 describe("Wikifog", function() {
+  beforeEach(function(){
+    loadFixtures("index.html");
+  });
+
+  describe("displaying errors", function(){
+
+    it("should display an error message when the ajax returns errors", function(){
+      spyOn($, "ajax").and.callFake(function(error) {
+        error.error({});
+      });
+
+      Wikifog.requestCloudData();
+
+      expect($('.notice-container')).not.toHaveClass('hidden');
+      expect($('.notice')).not.toBeEmpty()
+    });
+
+    it("should remove error message on next submit", function(){
+      spyOn($, "ajax");
+
+      $('.notice-container').removeClass('hidden');
+      $('.notice').html('<p>An Error Message</p>');
+
+      expect($('.notice-container')).not.toHaveClass('hidden');
+      expect($('.notice')).not.toBeEmpty()
+
+      Wikifog.requestCloudData();
+
+      expect($('.notice-container')).toHaveClass('hidden');
+      expect($('.notice')).toBeEmpty()
+    });
+  });
+
   describe("toggleMenu", function(){
     it("opens and closes the menu", function(){
-      loadFixtures("index.html");
       expect($('.dropdown-menu-content')).not.toHaveClass('hidden');
       $('.dropdown-menu-button').click();
       expect($('.dropdown-menu-content')).toHaveClass('hidden');
